25ed5d26c31eb536d00a65b74eb530a5e19c1b11
danstepanov <danhstepanov@gmail.com>
2020-04-28 20:09:49 -0700

    add support email, replace request PPE link, update total count, update donations

--- components/donations.jsx
+++ components/donations.jsx
@@ -24,7 +24,7 @@ export default ({ limit }) => {
       >
         <div className="flex items-baseline">
           <p className="text-4xl leading-8 mb-1 font-medium text-white ">
-            {data.raised ? data.raised : hasError ? '$30,812' : 'Loading...'}
+            {data.raised ? data.raised : hasError ? '$30,862' : 'Loading...'}
           </p>
         </div>
         <span className="flex justify-between">
--- pages/index.jsx
+++ pages/index.jsx
@@ -29,7 +29,7 @@ export default () => (
               <div className="rounded-md ">
                 {/* TODO Replace with LA info */}
                 <a
-                  href="https://bit.ly/nycmakesppe-request"
+                  href="https://bit.ly/crashcovid-request"
                   className=" flex items-center justify-center text-white px-2 py-3 bg-cool-gray-900 text-white font-medium rounded-lg"
                 >
                   Request PPE

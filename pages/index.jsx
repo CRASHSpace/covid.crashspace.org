eaa9d008a3afbad9f8988a552cef1b1180a63eec
quasarwei <quasar.wei@outlook.com>
2020-04-30 15:07:40 -0700

    add volunteer button

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default () => (
           </div>
         </div>
         <div className="relative m-0 w-full lg:max-w-sm mb-8 p-0 md:pt-12 lg:p-0">
-          <div className="flex flex-row flex-wrap md:flex-no-wrap justify-between rounded-lg">
+          <div className="flex flex-row flex-wrap justify-between rounded-lg">
             <div className="w-full rounded-lg bg-cool-gray-50 m-2 overflow-hidden flex flex-col justify-between p-6">
               <div className="items-baseline">
                 <div className="text-cool-gray-900 pb-3 items-baseline text-black text-xl md:text-2xl leading-tight font-semibold">
@@ -35,6 +35,21 @@ export default () => (
                 </a>
               </div>
             </div>
+            <div className="w-full rounded-lg bg-cool-gray-50 m-2 overflow-hidden flex flex-col justify-between p-6">
+              <div className="items-baseline">
+                <div className="text-cool-gray-900 pb-3 items-baseline text-black text-xl md:text-2xl leading-tight font-semibold">
+                  Do you want to help make a difference for those in need?
+                </div>
+              </div>
+              <div className="rounded-md ">
+                <a
+                  href="https://forms.gle/7rvuZP6h2qjGwS4d9"
+                  className=" flex items-center justify-center text-white px-2 py-3 bg-cool-gray-900 text-white font-medium rounded-lg"
+                >
+                  Volunteer
+                </a>
+              </div>
+            </div>
           </div>
           <div className="visible md:hidden pt-12 w-full mx-auto flex flex-wrap justify-start md:flex-nowrap">
             <TotalCount />
@@ -46,24 +61,36 @@ export default () => (
     <div className="max-w-3xl mx-auto pt-8">
       <div className="flex-1 mt-4 mb-6 p-4 rounded-lg">
         <p className="text-md leading-normal text-gray-700">
-We're mobilizing a collective effort of technologists and artists, hobbyists and aerospace engineers, science teachers and scientists, to join forces to respond to COVID-19. 
+          We're mobilizing a collective effort of technologists and artists,
+          hobbyists and aerospace engineers, science teachers and scientists, to
+          join forces to respond to COVID-19.
         </p>
         <p className="text-md leading-normal text-gray-700">
-We've shown our capacity to build a distributed manufacturing network across Los Angeles and produce thousands of approved personal protection equipment designs that were requested and put into use by clincians at local hospitals including Cedars-Sinai and USC Keck.
+          We've shown our capacity to build a distributed manufacturing network
+          across Los Angeles and produce thousands of approved personal
+          protection equipment designs that were requested and put into use by
+          clincians at local hospitals including Cedars-Sinai and USC Keck.
         </p>
-          <p className="text-md leading-normal text-gray-700">
-          We have the tools, the know-how, and the ability to rapidly respond to the challenges that are yet to come. But we need your help.
-          </p>
-   <p className="text-md leading-normal text-gray-700">
-   Are you a maker who wants to bring your skills to the cause, a medical professional in need of support, or a community member who wants to make a difference? Join our fight by donating your time, materials, or funding to protect those on the frontlines of COVID-19. 
+        <p className="text-md leading-normal text-gray-700">
+          We have the tools, the know-how, and the ability to rapidly respond to
+          the challenges that are yet to come. But we need your help.
         </p>
-     <p className="text-md leading-normal text-gray-700">
-       <b> By helping save their lives, we can help them save us all.</b>
+        <p className="text-md leading-normal text-gray-700">
+          Are you a maker who wants to bring your skills to the cause, a medical
+          professional in need of support, or a community member who wants to
+          make a difference? Join our fight by donating your time, materials, or
+          funding to protect those on the frontlines of COVID-19.
+        </p>
+        <p className="text-md leading-normal text-gray-700">
+          <b> By helping save their lives, we can help them save us all.</b>
         </p>
         <a className="text-base mr-4" href="/about">
           Learn More
         </a>
-        <a className="text-base" href="https://github.com/CRASHSpace/COVID-19-3dprints">
+        <a
+          className="text-base"
+          href="https://github.com/CRASHSpace/COVID-19-3dprints"
+        >
           Supported Designs
         </a>
       </div>

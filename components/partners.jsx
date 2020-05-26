1d8b5b2024347805033d541f667a844990c3b736
knarx <bensax@Benjamins-MacBook-Pro.local>
2020-05-26 00:34:00 -0700

    layout of most partners

--- components/partners.jsx
+++ components/partners.jsx
@@ -28,9 +28,61 @@ export default () => (
           src="/partner_logos/keckmedicine.png"
         />
       </span>
-
+      <span className=" overflow-hidden md:w-1/2">
+        <img
+          className="img-fluid"
+          src="/partner_logos/ucpla.png"
+        />
+      </span>
 
     </div>
+<span className="w-8 h-8"></span>
+
+        <div className="flex flex-wrap md:flex-no-wrap justify-center">
+      <span className=" overflow-hidden md:w-1/3">
+        <img
+          className="img-fluid"
+          src="/partner_logos/covenant.png"
+        />
+      </span>
+      <span className="w-8 h-8"></span>
+
+      <span className=" overflow-hidden md:w-1/3">
+        <img
+          className="img-fluid"
+          src="/partner_logos/protect_native_elders.png"
+        />
+      </span>
+           
+</div>
+
+<span className="w-8 h-8"></span>
+
+        <div className="flex flex-wrap md:flex-no-wrap justify-center">
+      <span className=" overflow-hidden md:w-1/4">
+        <img
+          className="img-fluid"
+          src="/partner_logos/sunweaver.png"
+        />
+      </span>
+      <span className="w-8 h-8"></span>
+
+      <span className=" overflow-hidden md:w-1/4">
+        <img
+          className="img-fluid"
+          src="/partner_logos/FRC.png"
+        />
+      </span>
+
+      <span className=" overflow-hidden md:w-1/4">
+        <img
+          className="img-fluid"
+          src="/partner_logos/palms.png"
+        />
+      </span>
+           
+</div>
+
 <span className="w-8 h-8"></span>
 
         <div className="flex flex-wrap md:flex-no-wrap justify-start">
@@ -54,7 +106,12 @@ export default () => (
           src="/partner_logos/3d.Printlife.Green.Black.hr.jpg"
         />
       </span>
-
+            <span className=" overflow-hidden md:w-1/3">
+        <img
+          className="img-fluid"
+          src="/partner_logos/Micro_Swiss_Logo_PNG.png"
+        />
+      </span>
 
     </div>
   </div>
